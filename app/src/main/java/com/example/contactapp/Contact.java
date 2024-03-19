6d1d5df829a91107663b7401f72d9d99c98c90bb
Long Vo <116516246+Longcodedao@users.noreply.github.com>
2024-03-19 13:49:40 +0700

    Second Add Code

---
+++ app/src/main/java/com/example/contactapp/Contact.java
@@ -0,0 +1,50 @@
+package com.example.contactapp;
+import androidx.room.ColumnInfo;
+import androidx.room.Entity;
+import androidx.room.PrimaryKey;
+
+@Entity(tableName = "my_contact")
+public class Contact {
+
+    @PrimaryKey(autoGenerate = true)
+    private int id;
+
+    @ColumnInfo
+    private String name;
+
+    @ColumnInfo
+    private String mobilePhone;
+
+    @ColumnInfo
+    private String email;
+
+    public Contact(String name, String mobilePhone, String email) {
+        this.name = name;
+        this.mobilePhone = mobilePhone;
+        this.email = email;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getMobilePhone() {
+        return mobilePhone;
+    }
+
+    public void setMobilePhone(String mobilePhone) {
+        this.mobilePhone = mobilePhone;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+}
--- app/src/main/java/com/example/contactapp/MainActivity.java
+++ app/src/main/java/com/example/contactapp/MainActivity.java
@@ -1,14 +1,40 @@
 package com.example.contactapp;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
 
 import android.os.Bundle;
+import android.view.View;
+
+import com.example.contactapp.databinding.ActivityMainBinding;
+
+import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
+
+    private ActivityMainBinding binding;
+
+    private ArrayList<Contact> contactList;
+    private ContactAdapter contactAdapter;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+
+        binding = ActivityMainBinding.inflate(getLayoutInflater());
+        View viewRoot = binding.getRoot();
+        setContentView(viewRoot);
+
+        binding.rvContacts.setLayoutManager(new LinearLayoutManager(this));
+        contactList = new ArrayList<>();
+        contactAdapter = new ContactAdapter(contactList);
+        binding.rvContacts.setAdapter(contactAdapter);
+
+        contactList.add(new Contact("Nguyen Van A", "0907042194", "a@gmail.com"));
+        contactList.add(new Contact("Nguyen Van B", "0907042194", "b@gmail.com"));
+        contactList.add(new Contact("Nguyen Van C", "0907042194", "c@gmail.com"));
+        contactAdapter.notifyDataSetChanged();
     }
+
 }
